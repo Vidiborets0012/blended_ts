0abcef6d20634c9cd56fd7fc680f9a601cc125e4
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-10-27 13:08:17 +0200

    add the overall structure of the project

---
+++ src/task-12.ts
@@ -0,0 +1,13 @@
+/*
+Задача 12
+
+Є функція sendDoneStatus:
+
+function sendDoneStatus(callback) {
+  callback("done");
+}
+
+Завдання:
+
+Типізуйте параметр callback, щоб це була функція, яка приймає рядок і повертав void.
+*/
